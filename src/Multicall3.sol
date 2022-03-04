4312da13bf42358ee6f40363e2cb4ae7e2d12dae
Andreas Bigger <abigger87@gmail.com>
2022-03-04 10:27:29 -0800

    :gear: payable dev note at top of contract

--- src/Multicall3.sol
+++ src/Multicall3.sol
@@ -4,11 +4,11 @@ pragma solidity 0.8.12;
 /// @title Multicall3
 /// @notice Aggregate results from multiple read-only function calls
 /// @dev Multicall & Multicall2 backwards-compatible
+/// @dev Aggregate methods are marked `payable` to save 24 gas per call
 /// @author Michael Elliot <mike@makerdao.com>
 /// @author Joshua Levine <joshua@makerdao.com>
 /// @author Nick Johnson <arachnid@notdot.net>
 /// @author Andreas Bigger <andreas@nascent.xyz>
-
 contract Multicall3 {
     struct Call {
         address target;
