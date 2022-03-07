7741318d2d6151c210bbf32602d27156f4718d2e
Matt Solomon <matt@mattsolomon.dev>
2022-03-06 16:33:12 -0800

    docs: update comments

--- src/Multicall3.sol
+++ src/Multicall3.sol
@@ -2,8 +2,7 @@
 pragma solidity 0.8.12;
 
 /// @title Multicall3
-/// @notice Aggregate results from multiple read-only function calls
-/// @notice Batches on-chain calls into a single transaction
+/// @notice Aggregate results from multiple function calls
 /// @dev Multicall & Multicall2 backwards-compatible
 /// @dev Aggregate methods are marked `payable` to save 24 gas per call
 /// @author Michael Elliot <mike@makerdao.com>
