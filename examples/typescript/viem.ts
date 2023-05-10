0a3f05441746cbc45996498d11efe02f488b84a0
Matt Solomon <matt@mattsolomon.dev>
2023-05-09 18:54:07 -0700

    feat: start improving viem example

--- examples/typescript/viem.ts
+++ examples/typescript/viem.ts
@@ -1,4 +1,4 @@
-import { createPublicClient, http } from 'viem';
+import { createPublicClient, http, parseAbi } from 'viem';
 import { mainnet } from 'viem/chains';
 import { MULTICALL_CONTRACT } from './constants';
 
@@ -7,8 +7,40 @@ const client = createPublicClient({
   transport: http(),
 });
 
+// Uniswap V3 constants.
+const UNISWAP_FACTORY = {
+  address: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
+  abi: parseAbi(['function getPool(address tokenA, address tokenB, uint24 fee) external view returns (address pool)'] as const)
+} as const
+
+const FEE_TIERS = [100, 500, 3000, 10000];
+
+// Tokens.
+const DAI_ADDRESS = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+
 async function example1() {
-  // TODO Why is the type of blockNumber `bigint | `0x${string}` | undefined` and not `bigint`?
+  // The first multicall computes the address of the DAI/USDC pool for each fee tier.
+  const calls = FEE_TIERS.map((fee) => {
+    return {
+      ...UNISWAP_FACTORY,
+      functionName: 'getPool',
+      args: [DAI_ADDRESS, USDC_ADDRESS, fee],
+    };
+  })
+
+  // Execute the multicall and get the results, where e.g. `ft100` is the address of the DAI/USDC
+  // pool for the 0.01% fee tier.
+  // NOTE Type inference does not work here.
+  const [ft100, ft500, ft3000, ft10000] = (await client.multicall({ contracts: calls })).map((v) => v.result);
+  console.log('ft100:  ', ft100);
+  console.log('ft500:  ', ft500);
+  console.log('ft3000: ', ft3000);
+  console.log('ft10000:', ft10000);
+
+  // ------------- other stuff --------
+
+  // NOTE Why is the type of blockNumber `bigint | `0x${string}` | undefined` and not `bigint`?
   // If I do `const blockNumber = await client.readContract({ ...MULTICALL_CONTRACT, functionName: 'getBlockNumber' });`
   // then the type is `bigint`, so not sure why the multicall version can't narrow the type further.
   const [blockNumber, currentBlockCoinbase] = (
@@ -32,6 +64,7 @@ async function example1() {
     `Coinbase: ${currentBlockCoinbase}`,
   ]);
 
+  // NOTE Type inference works fine here, it says blockNumber2 is `bigint`.
   const blockNumber2 = await client.readContract({
     ...MULTICALL_CONTRACT,
     functionName: 'getBlockNumber',
