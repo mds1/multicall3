a1a32fd34e28d07d50ad4e489b713a31316ec76f
Matt Solomon <matt@mattsolomon.dev>
2023-05-11 17:09:05 -0700

    feat: finish viem example

--- examples/typescript/viem.ts
+++ examples/typescript/viem.ts
@@ -1,13 +1,13 @@
-import { createPublicClient, http, parseAbi } from 'viem';
+import { createPublicClient, http, parseAbi, formatUnits } from 'viem';
 import { mainnet } from 'viem/chains';
-import { MULTICALL_CONTRACT } from './constants';
 
 const client = createPublicClient({
   chain: mainnet,
-  transport: http(),
+  transport: http(process.env.ETH_RPC_URL),
 });
 
 // Uniswap V3 constants.
+const FEE_TIERS = [100, 500, 3000, 10000];
 const UNISWAP_FACTORY = {
   address: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
   abi: parseAbi([
@@ -15,15 +15,13 @@ const UNISWAP_FACTORY = {
   ]),
 } as const;
 
-const FEE_TIERS = [100, 500, 3000, 10000];
-
-// Tokens.
+// Tokens addresses.
 const DAI_ADDRESS = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
 const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
 
 async function example1() {
   // The first multicall computes the address of the DAI/USDC pool for each fee tier.
-  const calls = FEE_TIERS.map((fee) => {
+  const poolAddrCalls = FEE_TIERS.map((fee) => {
     return {
       ...UNISWAP_FACTORY,
       functionName: 'getPool',
@@ -31,43 +29,58 @@ async function example1() {
     } as const;
   });
 
-  // Execute the multicall and get the results, where e.g. `ft100` is the address of the DAI/USDC
-  // pool for the 0.01% fee tier.
-  // NOTE Type inference does not work here.
-  const [ft100, ft500, ft3000, ft10000] = await client.multicall({ contracts: calls });
-  console.log('ft100:  ', ft100.result);
-  console.log('ft500:  ', ft500.result);
-  console.log('ft3000: ', ft3000.result);
-  console.log('ft10000:', ft10000.result);
+  // Execute the multicall and get the pool addresses. None of these calls can fail so we set
+  // `allowFailure` to false. This results in each return value's type matching the type of the
+  // corresponding call, e.g. `0x${string}` for addresses, `bigint` for uint256, etc. If we set
+  // `allowFailure` to true then the returns types are of the following shape, using the example of
+  // the address return type:
+  //   {
+  //       error: Error;
+  //       result?: undefined;
+  //       status: "error";
+  //   } | {
+  //       error?: undefined;
+  //       result: `0x${string}`;
+  //       status: "success";
+  //   }
+  const poolAddresses = await client.multicall({ contracts: poolAddrCalls, allowFailure: false });
+  console.log('DAI/USDC Pool Addresses');
+  const percentages = FEE_TIERS.map((fee) =>
+    (fee / 1e6).toLocaleString(undefined, {
+      style: 'percent',
+      minimumIntegerDigits: 1,
+      minimumFractionDigits: 2,
+    })
+  );
+  percentages.map((percent, i) => console.log(`  ${percent} pool: ${poolAddresses[i]}`));
 
-  // ------------- other stuff --------
-
-  // NOTE Why is the type of blockNumber `bigint | `0x${string}` | undefined` and not `bigint`?
-  // If I do `const blockNumber = await client.readContract({ ...MULTICALL_CONTRACT, functionName: 'getBlockNumber' });`
-  // then the type is `bigint`, so not sure why the multicall version can't narrow the type further.
-  const [blockNumber, currentBlockCoinbase] = await client.multicall({
-    contracts: [
-      {
-        ...MULTICALL_CONTRACT,
-        functionName: 'getBlockNumber',
-      },
-      {
-        ...MULTICALL_CONTRACT,
-        functionName: 'getCurrentBlockCoinbase',
-      },
-    ],
-  });
-
-  console.log([
-    `Contract Address: ${MULTICALL_CONTRACT.address}`,
-    `Block number: ${blockNumber.result}`,
-    `Coinbase: ${currentBlockCoinbase.result}`,
+  // For each pool, let's get the DAI and USDC balances.
+  const balanceOfAbi = parseAbi([
+    'function balanceOf(address who) external view returns (uint256 balance)',
   ]);
+  const DAI = { address: DAI_ADDRESS, abi: balanceOfAbi } as const;
+  const USDC = { address: USDC_ADDRESS, abi: balanceOfAbi } as const;
 
-  // NOTE Type inference works fine here, it says blockNumber2 is `bigint`.
-  const blockNumber2 = await client.readContract({
-    ...MULTICALL_CONTRACT,
-    functionName: 'getBlockNumber',
+  const balanceCalls = poolAddresses
+    .map((poolAddress) => {
+      return [
+        { ...DAI, functionName: 'balanceOf', args: [poolAddress] } as const,
+        { ...USDC, functionName: 'balanceOf', args: [poolAddress] } as const,
+      ];
+    })
+    .flat();
+
+  // Execute the multicall and log the results.
+  const balances = await client.multicall({ contracts: balanceCalls, allowFailure: false });
+
+  console.log('DAI/USDC Pool Balances');
+  balances.map((balance, i) => {
+    const token = i % 2 === 0 ? 'DAI' : 'USDC';
+    const decimals = i % 2 === 0 ? 18 : 6;
+    const percent = percentages[Math.floor(i / 2)];
+    const amount = Number(formatUnits(balance, decimals)).toLocaleString(undefined, {});
+    const spacer = ' '.repeat(5 - token.length);
+    console.log(`  ${percent} pool ${token} balance:${spacer}${amount}`);
   });
 }
 
